3d58a1d10912b73dba965d84ea05baad3ce9d144
YahiaEldakhakhny <yahia4360673@gmail.com>
2024-06-25 14:03:02 +0300

    stereo camera works

--- mjpg_launch.sh
+++ mjpg_launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/mjpg-streamer/mjpg-streamer-experimental
 export LD_LIBRARY_PATH=.
-./mjpg_streamer -i input_uvc.so -o "output_http.so -w ./www --listen 0.0.0.0"
+./mjpg_streamer -i "input_uvc.so -d /dev/video0 -r 1280x480" -o "output_http.so -w ./www --listen 0.0.0.0"
